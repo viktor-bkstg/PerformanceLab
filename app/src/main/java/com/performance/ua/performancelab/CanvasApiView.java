1018516211e14a857f3766091ed9f9e001f174ad
Viktor Matiash <viktor@bkstg.com>
2016-04-23 12:38:22 +0300

    lab 1

--- app/src/main/java/com/performance/ua/performancelab/CanvasApiView.java
+++ app/src/main/java/com/performance/ua/performancelab/CanvasApiView.java
@@ -33,10 +33,15 @@ public class CanvasApiView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         for (int i = 0; i < N; i++) {
+            canvas.save();
             // Each card is laid out a little to the right of the previous one.
             myPaint.setColor(Color.RED / (i + 1));
             myPaint.setStrokeWidth(10);
+            if (i < N-1) {
+                canvas.clipRect(MARGIN + i * shift, MARGIN, MARGIN + shift + i * shift, SIZE);
+            }
             canvas.drawRect(MARGIN + i * shift, MARGIN, SIZE + i * shift, SIZE, myPaint);
+            canvas.restore();
         }
         // Invalidate the whole view. Doing this calls onDraw() if the view is visible.
         invalidate();
